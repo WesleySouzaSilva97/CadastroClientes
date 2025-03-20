611d2c918aebbfb1a056b623e0248aa9d065e0a5
Wesley Souza <wesleyondo@gmail.com>
2025-03-20 20:41:02 -0300

    att

--- main.js
+++ main.js
@@ -6,7 +6,13 @@ console.log("Electron processo principal")
 // nativeTheme está relacionado ao tema claro ou escuro (definir) 
 // Menu (definir um menu personalizado )
 // shell (acessar links externos no navegador padrão)
-const { app, BrowserWindow, nativeTheme, Menu, shell } = require('electron/main')
+const { app, BrowserWindow, nativeTheme, Menu, shell, ipcMain } = require('electron/main')
+
+// Ativação do preload.js (importação do path (caminho))
+const path = require('node:path')
+
+// Importação ds métodos conectar e desconectar (módulo de conexão)
+const { conectar, desconectar } = require('./database.js')
 
 // Janela principal
 let win
@@ -16,48 +22,65 @@ const createWindow = () => {
   win = new BrowserWindow({
     width: 1010, // Largura
     height: 720, // Altura
-    // frame: false
     resizable: false, // Maximizar
-    // minimizable: false, // Minimizar
-    closable: false, //fecheible
-    // autoHideMenuBar: true // fesconder o menu
+
+      // Linhas abaixo para ativação do preload. Importado através da linha de Importação ds métodos conectar e desconectar (módulo de conexão)
+  webPreferences: {
+    preload: path.join(__dirname, 'preload.js')
+  }
   })
 
   // Carregar o menu personalizado
-  // Atenção!!!! Antes importar o recurso menu
+  // Atenção! Antes importar o recurso Menu
   Menu.setApplicationMenu(Menu.buildFromTemplate(template))
 
-  // carregar o documento na janela
-  win.loadFile('./src/views/index.html')
+
+// Carregar o documento HTML na janela
+win.loadFile('./src/views/index.html')
 }
 
-//janela sobre
+// Janela sobre
 let about
 function aboutWindow() {
   nativeTheme.themeSource = 'light'
-  // obter a janela principal 
+  // Obter a janela principal
   const mainWindow = BrowserWindow.getFocusedWindow()
-  // validação (se existir a janela principal)
+  // Validação (se existir a janela principal)
   if (mainWindow) {
     about = new BrowserWindow({
-      width: 320,
-      height: 290,
-      autoHideMenuBar: true,
-      resizable: false,
-      minimizable: false,
-      parent: mainWindow,  // estabelecer uma relação hierárquica entre janelas 
-      modal: true
+      width: 700, // Largura
+      height: 550, // Altura
+      autoHideMenuBar: true, // Esconder o menu do browser
+      resizable: false, // Maximizar
+      minimizable: false, // Minimizar
+      parent: mainWindow, // Estabelecer uma relação hierárquica entre janelas
+      modal: true // Criar uma janela modal
     })
   }
-  
+
   about.loadFile('./src/views/sobre.html')
 }
 
-// inicialização da aplicação 
+// Inicialização da aplicação (assincronismo)
 app.whenReady().then(() => {
   createWindow()
 
-  // só ativar a janela principal se nenhuma outra estiver ativa
+  // Melhor local para estebelecer a conexão com o banco de dados
+  // No MongoDb é mais eficiente manter uma única conexão aberta durante todo o tempo de vida do aplicativo e fechar a conexão e encerrar quando o aplicativo for finalizado
+  // ipcMain.on (receber mensagem)
+  // db-connect (rótulo da mensagem)
+  ipcMain.on('db-connect', async (event) => {
+    // A linha abaixo estabelece a conexão com o banco de dados
+    await conectar()
+    // Enviar ao rendereizador uma mensagem para trocar a imagem do ícone do status do banco de dados (criar um delay de 0.5s ou 1s para sincronização com a nuvem)
+    setTimeout(() => {
+      // Enviar ao renderizador a mensagem "conectado"
+      // db-status (IPC - comunicação entre processos - autorizada pelo preload.js)
+      event.reply('db-status', "conectado")
+    }, 500) // 500ms = 0.5s
+  })
+
+  // Só ativar a janela principal se nenhuma outra estiver ativa
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow()
@@ -65,24 +88,30 @@ app.whenReady().then(() => {
   })
 })
 
-// se o sistema não for MAC encerrar a aplicação quando a janela for fechada
+// Se o sistema não for MAC, encerrar a aplicação quando a janela for fechada
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
-// reduzir a verbosidade de logs não críticos  (devtools)
+// IMPORTANTE! Desconectar do banco de dados quando a aplicação for finalizada
+app.on('before-quit', async () => {
+  await desconectar()
+})
+
+// Reduzir a verbosidade de logs não críticos (devtools)
 app.commandLine.appendSwitch('log-level', '3')
 
-// template do menu  
+// Template do menu
+// Abertura e fechamento em [] é para a criação de um vetor
 const template = [
   {
     label: 'Cadastro',
     submenu: [
       {
         label: 'Sair',
-        accelerator: 'Ctrl+N',
+        accelerator: 'Alt+F4',
         click: () => app.quit()
       }
     ]
@@ -107,11 +136,15 @@ const template = [
         role: 'zoomOut'
       },
       {
-        label: 'Restaurar o zoom padrão',
+        label: 'Restaurar zoom padrão',
         role: 'resetZoom'
       },
       {
         type: 'separator'
+      },
+      {
+        label: 'Recarregar',
+        role: 'reload'
       },
       {
         label: 'DevTools',
